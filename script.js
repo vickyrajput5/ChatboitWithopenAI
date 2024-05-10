29c2add499e6feecf730463c1facaa0b606f907c
Muhammad Waqar <58720926+vickyrajput5@users.noreply.github.com>
2024-05-10 17:33:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-// const API_KEY = ""; // Paste your API key here
+const API_KEY = "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";
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
@@ -27,7 +27,7 @@ const generateResponse = (chatElement) => {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${process.env.API_KEY}`
+            "Authorization": `Bearer ${API_KEY}`
         },
         body: JSON.stringify({
             model: "gpt-3.5-turbo",
